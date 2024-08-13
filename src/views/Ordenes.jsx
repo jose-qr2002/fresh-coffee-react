b6560b5e94c60dd43286f5f039418aca6963d76b
Jose Quispe <jkqr2016@gmail.com>
2024-08-12 20:05:53 -0500

    Consultando los pedidos

--- src/views/Ordenes.jsx
+++ src/views/Ordenes.jsx
@@ -1,5 +1,22 @@
+import useSWR from "swr"
+import clienteAxios from "../config/axios"
+
 export default function Ordenes() {
-  return (
+    const token = localStorage.getItem('AUTH_TOKEN');
+    const fetcher = () => clienteAxios('/api/pedidos', {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+
+    // USE SWR revalidad lo del fetcher
+    const { data, error, isLoading } = useSWR('/api/pedidos', fetcher)
+
+    console.log(data);
+    console.log(error);
+    console.log(isLoading);
+    
+    return (
     <div>
         <h1 className='text-4xl font-black'>Ordenes</h1>
         <p className='text-2xl my-10'>

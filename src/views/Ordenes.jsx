6abbc38034890d302801cda028577fdc4e9da6d2
Jose Quispe <jkqr2016@gmail.com>
2024-08-13 15:12:17 -0500

    Mostrando cliente y total

--- src/views/Ordenes.jsx
+++ src/views/Ordenes.jsx
@@ -1,5 +1,6 @@
 import useSWR from "swr"
 import clienteAxios from "../config/axios"
+import {formatearDinero} from "../helpers"
 
 export default function Ordenes() {
     const token = localStorage.getItem('AUTH_TOKEN');
@@ -21,7 +22,7 @@ export default function Ordenes() {
             Administra las ordenes desde aqui.    
         </p>
 
-        <div>
+        <div className="grid grid-cols-2">
             {data.data.data.map(pedido => (
                 <div key={pedido.id} className="p-5 bg-white shadow space-y-2 border-b">
                     <p className="text-xl font-bold text-slate-600">
@@ -37,6 +38,20 @@ export default function Ordenes() {
                             </p>
                         </div>
                     ))}
+                    <p className="text-lg font-bold text-slate-600">
+                        Cliente: {''}
+                        <span className="font-normal">{pedido.user.name}</span>
+                    </p>
+                    <p className="text-lg font-bold text-amber-600">
+                        Total a pagar: {''} 
+                        <span className="font-normal text-slate-600">{ formatearDinero( pedido.total )}</span>
+                    </p>
+                    <button
+                        type="button" 
+                        className='bg-indigo-600  hover:bg-indigo-800 px-5 py-2 rounded uppercase font-bold text-white text-center w-full cursor-pointer'
+                    >
+                        Completar
+                    </button>
                 </div>
             ))}
         </div>
